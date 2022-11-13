e082788da1e61cdd173d6c41e0382fdc370317f1
Srinivas0000 <nivasniyas24@gmail.com>
2022-11-13 10:48:35 +0530

    app built

---
+++ src/components/Header.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Image from 'next/image'
+import {
+  Search,
+  ShoppingCartOutline,
+  MenuOutline
+} from 'heroicons-react'
+
+function Header() {
+  return (
+    <header>
+      {/* Top Header */}
+      <div className='flex items-center bg-amazon_blue p-1 flex-grow py-2'>
+        <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
+          <Image
+            src='https://links.papareact.com/f90'
+            width={150}
+            height={40}
+            objectFit="contain"
+            className='cursor-pointer'
+          />
+        </div>
+
+        {/* Search */}
+        <div className='hidden sm:flex items-center h-10 rounded-md flex-grow bg-yellow-400 hover:bg-yellow-500'>
+          <input type="text" className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4'/>
+          <Search className='h-12 m-3'/>
+        </div>
+
+        {/* Right */}
+        <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+          <div className='link'>
+            <p>Hellow Nivas</p>
+            <p className='font-extrabold md:text-sm'>Account & Lists</p>
+          </div>
+          <div className='link'>
+            <p>Returns</p>
+            <p className='font-extrabold md:text-sm'>& Orders</p>
+          </div>
+          <div className='relative link flex items-center'>
+            <span className='absolute top-0 right-0  md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
+            <ShoppingCartOutline className='h-10'/>
+            <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
+          </div>
+        </div>
+      </div>
+      {/* Bottom Header */}
+      <div className='flex items-center bg-amazon_blue-light space-x-3 text-white'>
+        <p className='link flex items-center'>
+          <MenuOutline className='h-6 mr-1' />
+          All
+        </p>
+        <p className='link'>Prime Video</p>
+        <p className='link'>Amazon Business</p>
+        <p className='link'>Today's Deals</p>
+        <p className='link hidden lg:inline-flex'>Electronics</p>
+        <p className='link hidden lg:inline-flex'>Food & Grocery</p>
+        <p className='link hidden lg:inline-flex'>Prime</p>
+        <p className='link hidden lg:inline-flex'>Buy Again</p>
+        <p className='link hidden lg:inline-flex'>Shopper Toolkit</p>
+        <p className='link hidden lg:inline-flex'>Health & Personal Care</p>
+      </div>
+    </header>
+  )
+}
+
+export default Header
